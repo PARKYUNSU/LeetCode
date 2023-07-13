8c06305b6626d6b1c88ae83b847b91173505d4b3
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2023-07-13 17:32:48 +0900

    Time: 559 ms, Memory: 31 MB - LeetHub

--- contains-duplicate/contains-duplicate.py
+++ contains-duplicate/contains-duplicate.py
@@ -1,7 +1,4 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         new_nums=list(set(nums)) #set은 중복 값을 허용하지 않음을 이용
-        if len(new_nums) != len(nums): #중복 값을 제거한 new_nums와 기존 nums 길이가 다르면 중복 값이 있으므로 True
-            return True
-        else:
-            return False
+        return len(new_nums) != len(nums) #중복 값을 제거한 new_nums와 기존 nums 길이가 다르면 중복 값이 있으므로 True
