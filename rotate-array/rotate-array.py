7f5decd377a2dd300d155467dfe4330b7797415b
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2023-07-13 17:06:03 +0900

    Time: 224 ms, Memory: 27.6 MB - LeetHub

--- rotate-array/rotate-array.py
+++ rotate-array/rotate-array.py
@@ -1,4 +1,4 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        k = k % len(nums)
-        nums[:] = nums[-k:] + nums[:-k]
+        k = k % len(nums) #k가 nums리스트 보다 클 경우 예를들어, nums=[1,2,3], k=5 에서 k를 nums의 길이로 나누어 몫을 새로운 k로 할당
+        nums[:] = nums[-k:] + nums[:-k] #nums의 전체리스트는 k기준으로 슬라이싱
